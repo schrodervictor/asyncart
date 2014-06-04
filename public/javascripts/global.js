6afad11003494f18f9c0c7566ca43c3344611b89
Victor Schröder <schrodervictor@gmail.com>
2014-06-04 22:18:37 +0200

    Finish tutorial 2 with RESTful API

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -2,12 +2,16 @@ var userListData = [];
 
 $(document).ready(function(){
 	populateTable();
+	$('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+	$('#btnAddUser').on('click', addUser);
+	$('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser)
 });
 
 function populateTable() {
 	var tableContent = '';
 
 	$.getJSON('/users/userlist', function(data) {
+		userListData = data;
 		$.each(data, function(){
 			tableContent += '<tr>';
 			tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show details">' + this.username + '</a></td>';
@@ -19,3 +23,92 @@ function populateTable() {
 		$('#userList table tbody').html(tableContent);
 	});
 };
+
+function showUserInfo(event) {
+
+	event.preventDefault();
+	var thisUserName = $(this).attr('rel');
+	var arrayPosition = userListData
+		.map(function(arrayItem) {
+			return arrayItem.username;
+		})
+		.indexOf(thisUserName);
+
+	var thisUserObject = userListData[arrayPosition];
+
+	$('#userInfoName').text(thisUserObject.fullname);
+	$('#userInfoAge').text(thisUserObject.age);
+	$('#userInfoGender').text(thisUserObject.gender);
+	$('#userInfoLocation').text(thisUserObject.location);
+};
+
+function addUser(event) {
+	event.preventDefault();
+
+	var errorCount = 0;
+	
+	$('#addUser input').each(function(index, val) {
+		if ($(this).val() === '') {
+			errorCount++;
+		};
+	});
+
+	if (errorCount === 0) {
+
+		var newUser = {
+			'username': $('#addUser fieldset input#inputUserName').val(),
+			'email': $('#addUser fieldset input#inputUserEmail').val(),
+			'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+			'age': $('#addUser fieldset input#inputUserAge').val(),
+			'location': $('#addUser fieldset input#inputUserLocation').val(),
+			'gender': $('#addUser fieldset input#inputUserGender').val()
+		};
+
+		$.ajax({
+			type: 'POST',
+			data: newUser,
+			url: '/users/adduser',
+			dataType: 'JSON'
+		}).done(function(response) {
+			if (response.msg === '') {
+				$('#addUser fieldset input').val('');
+				populateTable();
+			} else {
+				alert('Error: ' + response.msg);
+			}
+		});
+
+	} else {
+		alert('Please fill in all fields');
+		return false;
+	}
+};
+
+function deleteUser(event) {
+	event.preventDefault();
+
+	var confirmation = confirm('Are you sure you want to delete this user?');
+
+	if (confirmation === true) {
+
+		$.ajax({
+			type: 'DELETE',
+			url: '/users/deleteuser/' + $(this).attr('rel')
+		}).done(function(response) {
+			if (response.msg === '') {
+
+			} else {
+				alert('Error: ' + response.msg);
+			}
+
+			populateTable();
+		});
+
+	} else {
+		return false;
+	}
+
+};
+
+
+
--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,27 @@ router.get('/userlist', function(req, res) {
   });
 });
 
+/* POST to adduser */
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+  db.collection('users')
+  	.insert(req.body, function(err, result) {
+  		res.send(
+  			(err === null) ? {msg: ''} : {msg:err}
+  		);
+  	});
+});
+
+/* DELETE to deleteuser */
+router.delete('/deleteuser/:id', function(req, res) {
+  var db = req.db;
+  var userToDelete = req.params.id;
+  db.collection('users')
+  	.removeById(userToDelete, function(err, result) {
+  		res.send(
+  			(result === 1) ? {msg: ''} : {msg:err}
+  		);
+  	});
+});
+
 module.exports = router;

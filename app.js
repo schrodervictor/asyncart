3f7fc69c87452709f5c2d2297eb614633a34834e
Victor Schröder <schrodervictor@gmail.com>
2014-06-04 19:17:55 +0200

    Tutorial from cwbuecheler.com

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//DB
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodecart');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +26,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// DB link
+app.use(function(req,res,next) {
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,49 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Hello World. */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Hello World!' });
+});
+
+/* GET New User Form. */
+router.get('/newuser', function(req, res) {
+  res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST Add User. */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    var collection = db.get('users');
+
+    collection.insert({
+        "username": userName,
+        "email": userEmail
+    }, function(err, doc) {
+        if (err) {
+            res.send('There was a problem adding information do DB');
+        } else {
+            res.location('userlist');
+            res.redirect('userlist');
+        }
+    });
+});
+
+/* GET userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('users');
+    console.log(collection);
+    collection.find({},{}, function(err, docs) {
+        console.log(docs);
+        res.render('userlist', {
+            "userlist": docs
+        });
+    });
+});
+
 module.exports = router;

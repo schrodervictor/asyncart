c7728ac3ae14ffd9eceb8d4245581bcb6194b04f
Victor Schröder <schrodervictor@gmail.com>
2014-06-05 22:23:28 +0200

    Begin MVC Tutorial for Node.js by Krasimir Tsonev

--- app.js
+++ app.js
@@ -12,13 +12,14 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hjs');
 
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);

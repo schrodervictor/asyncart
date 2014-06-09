7b62cdc1b028392b12dcbcfb6d018cda1d5d1313
Victor Schröder <schrodervictor@gmail.com>
2014-06-09 19:39:21 +0200

    Modifies View.class to the new Extendable.class

--- engine/View.class.js
+++ engine/View.class.js
@@ -1,26 +1,15 @@
-var View = function(res, template) {
-	this.res = res;
-	this.template = template;
-};
-View.prototype = {
-	extend: function(properties) {
-		var Child = View;
-		Child.prototype = View.prototype;
-		for(var key in properties) {
-			Child.prototype[key] = properties[key];
-		}
-		return Child;
+var Extendable = require('./Extendable.class');
+
+var View = (new Extendable()).extend({
+	init: function(res, template) {
+		this.res = res;
+		this.template = template;
 	},
 	render: function(data) {
 		if(this.res && this.template) {
 			this.res.render(this.template, data);
 		}
 	}
-}
-
-// Static method
-View.getInstance = function(res, template) {
-	return new View(res, template);
-}
+});
 
 module.exports = View;

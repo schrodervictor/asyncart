479e58565277fc8880712f0e2c65ab6d14f0f016
Victor Schröder <schrodervictor@gmail.com>
2014-06-11 03:32:39 +0200

    Uses the new AdminController in the router/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var config = require('../config')();
 var express = require('express');
 var router = express.Router();
+var Admin = require('../controller/AdminController');
 
 
 
@@ -12,6 +13,24 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express 222' });
 	//next();
 });
-	
+
+/* POST Admin Login page */
+router.post('/admin/login', function(req, res, next) {
+	var admin = new Admin();
+	admin.run(req, res, next);
+});
+
+/* All Admin Logout */
+router.all('/admin/logout', function(req, res, next) {
+	var admin = new Admin();
+	admin.logout(req, res, next);
+});
+
+/* GET Admin page */
+router.get('/admin*', function(req, res, next) {
+	var Admin = require('../controller/AdminController');
+	var admin = new Admin();
+	admin.run(req, res, next);
+});
 
 module.exports = router;

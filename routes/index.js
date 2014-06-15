28124b226f21d05805bed8e425b5fb845a2d00b0
Victor Schröder <schrodervictor@gmail.com>
2014-06-15 03:20:56 +0200

    Add route test for tests in the Browser

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var config = require('../config')();
 var express = require('express');
 var router = express.Router();
+var ContentController = require('../controller/ContentController.class');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +10,11 @@ router.get('/', function(req, res, next) {
 	});
     res.render('index', { title: 'Express 222' });
 	//next();
+});
+
+router.get('/teste', function(req, res, next) {
+	content = new ContentController();
+	content.run(req, res, next);
 });
 
 module.exports = router;

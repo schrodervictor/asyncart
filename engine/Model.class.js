6e803c3bf7e9d346719c9a3e31934d1f972cf612
Victor Schröder <schrodervictor@gmail.com>
2014-06-22 18:44:53 +0200

    Remove unnecessary methods in Model.class.js

--- engine/Model.class.js
+++ engine/Model.class.js
@@ -1,16 +1,9 @@
 var Extendable = require('./Extendable.class');
 
 var Model = (new Extendable()).extend({
-	init: function(db) {
-		this.db = db;
-	},
 	setDB: function(db) {
 		this.db = db;
 	},
-	collection: function() {
-		if(this._collection) return this._collection;
-		return this._collection = this.db.collection('fastdelivery-content');
-	}
 });
 
 

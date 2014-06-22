7ca70318a581cc811cdaa3af710bc56f8cd392a0
Victor Schröder <schrodervictor@gmail.com>
2014-06-22 21:18:44 +0200

    Changes tabs to spaces in HeaderController

--- controller/common/HeaderController.class.js
+++ controller/common/HeaderController.class.js
@@ -2,79 +2,78 @@ var Controller = require('../../engine/Controller.class');
 var async = require('async');
 
 var HeaderController = (new Controller()).extend({
-	exposedActions: {
-		init: false
-	},
-	init: function() {
-		this.req = arguments[0];
-		this.res = arguments[1];
-		this.next = arguments[2];
+    exposedActions: {
+        init: false,
+    },
+    init: function() {
+        this.req = arguments[0];
+        this.res = arguments[1];
+        this.next = arguments[2];
 
-		this.groupName = 'common';
-		this.controllerName = 'header';
-		this.route = 'common/header';
-		this.template = 'common/header';
-		this.data = {};
-	},
-	
-	renderAsPartial: function(origCallback) {
-		var self = this;
+        this.groupName = 'common';
+        this.controllerName = 'header';
+        this.route = 'common/header';
+        this.template = 'common/header';
+        this.data = {};
+    },
+    renderAsPartial: function(origCallback) {
+        var self = this;
 
-		self.data = {
-			title: 'Second title',
-		};
+        self.data = {
+            title: 'Second title',
+        };
 
-		self.modelCustomer = new (require('../../model/account/CustomerModel.class'))(self.req, self.res);
+        self.modelCustomer = new (require('../../model/account/CustomerModel.class'))(self.req, self.res);
 
-		async.waterfall([
+        async.waterfall([
 
-		    function(callback) {
+            function(callback) {
 
-		    	var customerData = {
-					firstname: 'Victor',
-					lastname: 'Schröder',
-					email: 'schrodervictor@gmail.com',
-					telephone: '+00 000 0000 0000',
-					password: 'teste',
-					cart: {},
-					wishlist: {},
-					newsletter: true,
-					addresses: [
-						{  },
-						{  }
-					],
-					groups: ['default'],
-					ips: ['127.0.0.1'],
-					additionalFields: {},
-				};
+                var customerData = {
+                    firstname: 'Victor',
+                    lastname: 'Schröder',
+                    email: 'schrodervictor@gmail.com',
+                    telephone: '+00 000 0000 0000',
+                    password: 'teste',
+                    cart: {},
+                    wishlist: {},
+                    newsletter: true,
+                    addresses: [
+                        {  },
+                        {  }
+                    ],
+                    groups: ['default'],
+                    ips: ['127.0.0.1'],
+                    additionalFields: {},
+                };
 
-				self.modelCustomer.addCustomer(customerData, function(err, result){
-					if(err) return callback(err);
-					self.res.locals.newId = result[0]._id;
-		        	callback();
-				});
-		    
-		    },
-		    function(callback) {
+                self.modelCustomer.addCustomer(customerData, function(err, result){
+                    if(err) return callback(err);
+                    self.res.locals.newId = result[0]._id;
+                    callback();
+                });
+            
+            },
+            function(callback) {
 
-				//self.modules = self.model.getModules();
-				self.modules = {
-					cart: 'module/cart',
-					language: 'module/language',
-					currency: 'module/currency',
-				};
+                //self.modules = self.model.getModules();
+                self.modules = {
+                    cart: 'module/cart',
+                    language: 'module/language',
+                    currency: 'module/currency',
+                };
 
-				self.renderedPartials(self.modules, function(err, result){
-					callback();
-				});
-		    	
-		    }
-		], function(err) {
-			if(err) return origCallback(err);
-			self.render(origCallback);
-		});
+                self.renderedPartials(self.modules, function(err, result){
+                    callback();
+                });
+                
+            }
+        ], function(err) {
+            if(err) return origCallback(err);
+            self.render(origCallback);
+        });
 
-	},
+    },
 });
 
 module.exports = HeaderController;

ed9ba5cb37551a03ec77967b118b2f63b23e0c56
Victor Schröder <schrodervictor@gmail.com>
2014-06-29 23:36:09 +0200

    Rewrites the routes in admin/router

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,7 +1,74 @@
 var config = require(__config)();
 var express = require('express');
 var router = express.Router();
-var Admin = require('../controller/admin/AdminController.class');
+
+router.param('group', function(req, res, next, group) {
+	// TODO implement validation of this entry (for safety)
+    req.group = group.toLowerCase();
+    console.log('Group: ' + req.group);
+    next();
+});
+
+router.param('controller', function(req, res, next, controller) {
+	// TODO implement validation of this entry (for safety)
+	// TODO Add suport for multiple word controller files
+    req.controller = controller.charAt(0).toUpperCase() + controller.substr(1).toLowerCase();
+    if(req.group) {
+    	req.controllerPath = req.group + '/' + req.controller;
+    }
+    console.log('Controller: ' + req.controller);
+    next();
+});
+
+router.param('action', function(req, res, next, action) {
+	// TODO implement validation of this entry (for safety)
+	// TODO Add suport for multiple word methods
+   	req.action = action.toLowerCase();
+    console.log('Action: ' + req.action);
+    next();
+});
+
+router.use(function isLogged(req, res, next) {
+	req.user = req.load.engine('user');
+	req.user.isLogged(function(err, logged) {
+		if(err) return next(err);
+		if(logged) return next();
+		var controller = req.load.engine('controller');
+		return controller.forward('admin/login');
+	});
+});
+
+router.use(function hasPermission(req, res, next) {
+	req.user.hasPermission(function(err, permission) {
+		if(err) return next(err);
+		if(permission) return next();
+		return next({status: 407, message: 'Forbidden'});
+	});
+});
+
+/* GET home page. */
+router.get('/:group/:controller/:action?', function(req, res, next) {
+	var page = req.load.controller(req.controllerPath);
+	if(page.isExposedAction(req.action || 'index')) {
+		page[req.action || 'index']();
+	} else {
+		next();
+	}
+});
+
+/* POST home page. */
+router.post('/:group/:controller/:action?', function(req, res, next) {
+	var page = req.load.controller(req.controllerPath);
+	if(page.isExposedAction(req.action || 'index')) {
+		page[req.action || 'index']();
+	} else {
+		next();
+	}
+});
+
+
+
+var Admin = require('../controller/catalog/AdminController.class');
 
 /* All Admin Login page */
 router.all('/login', function(req, res, next) {

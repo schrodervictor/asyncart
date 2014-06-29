bef9cb9fc4eeeab5c08889cc0b4bfe8df2a833bd
Victor Schröder <schrodervictor@gmail.com>
2014-06-29 23:19:58 +0200

    Writes the entry point for the mounted apps: store and admin

---
+++ admin/index.js
@@ -0,0 +1,19 @@
+var config = require(__config)();
+var express = require('express');
+var path = require('path');
+
+var admin = express();
+
+admin.use(function(req, res, next) {
+    req.loadPoint = __dirname;
+    next();
+});
+
+// view engine setup
+var templateName = 'default';
+admin.set('views', path.join(__dirname, 'view/template', templateName));
+admin.set('view engine', 'hjs');
+
+admin.use('/', require('./routes/index'));
+
+module.exports = admin;
--- store/index.js
+++ store/index.js
@@ -1,67 +1,30 @@
-var config = require('./config')();
+var config = require(__config)();
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var cookieSession = require('cookie-session');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var database = require('./engine/Database');
-var sessionStore = require('./engine/sessionStore.obj');
-var loader = require('./engine/Loader.class');
-var cart = require('./engine/Cart.class');
+var cart = require(config.enginePath + '/Cart.class');
 
-var app = express();
+var store = express();
+
+store.use(function(req, res, next) {
+    req.loadPoint = __dirname;
+    next();
+});
 
 // view engine setup
 var templateName = 'default';
-app.set('views', path.join(config.templatePath, templateName));
-app.set('view engine', 'hjs');
+store.set('views', path.join(__dirname, 'view/template', templateName));
+store.set('view engine', 'hjs');
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(session({
-    secret: config.session.secret,
-    cookie: { maxAge: config.cookie.maxAge },
-    store: sessionStore
-}));
-//app.use(cookieSession({
-//    keys: ['key1', 'key2']
-//}));
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
+//store.use(require('less-middleware')(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'public')));
+store.use(cart);
 
-//TODO Try to make cache works without this line
-app.disable('etag');
-
+store.use('/a', require('./routes/ajax'));
+store.use('/', require('./routes/index'));
 
 /*
-groups = [
-    'admin',
-    'common',
-    'users',
-];
-
-for (var i = 0, j = groups.length; i < j; i++) {
-    if(groups[i] === 'common') {
-        app.use('/', require('./routes/index'));        
-    } else {
-        app.use('/' + groups[i], require('./routes/' + groups[i]));
-    }
-};*/
-app.use(loader);
-app.use(cart);
-app.use(database());
-app.use('/admin', require('./routes/admin'));
-app.use('/', require('./routes/index'));
-
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+store.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -71,8 +34,8 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+if (store.get('env') === 'development') {
+    store.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -83,7 +46,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+store.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -91,5 +54,5 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
-module.exports = app;
+*/
+module.exports = store;
